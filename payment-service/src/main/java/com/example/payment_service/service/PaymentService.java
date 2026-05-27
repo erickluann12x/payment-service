502343cad5826c52761b9fa1bd835f1f4ca3ae88
ErickLuan <erickluan12x@gmail.com>
2026-05-26 22:33:36 -0300

    Create logic bussiness/createPayment/findALLpayment

---
+++ payment-service/src/main/java/com/example/payment_service/service/PaymentService.java
@@ -0,0 +1,43 @@
+package com.example.payment_service.service;
+
+import com.example.payment_service.dto.PaymentRequestDTO;
+import com.example.payment_service.dto.PaymentResponseDTO;
+import com.example.payment_service.entity.Payment;
+import com.example.payment_service.entity.PaymentStatus;
+import com.example.payment_service.mapper.PaymentMapper;
+import com.example.payment_service.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+
+    public PaymentResponseDTO createPayment(PaymentRequestDTO paymentRequestDTO) {
+        Payment payment = new Payment();
+
+        payment.setOrderId(payment.getOrderId());
+        payment.setAmount(payment.getAmount());
+        payment.setMethod(payment.getMethod());
+        payment.setCustomerEmail(payment.getCustomerEmail());
+
+
+        payment.setStatus(PaymentStatus.PENDING);
+
+        payment.setStatus(PaymentStatus.APPROVED);
+
+        Payment savedPayment = paymentRepository.save(payment);
+
+        return paymentMapper.ToResponse(savedPayment);
+
+    }
+    public List<PaymentResponseDTO> findAllPayments(){
+
+        List<Payment> payments = paymentRepository.findAll();
+        return payments.stream().map(paymentMapper::ToResponse).toList();
+    }
+}

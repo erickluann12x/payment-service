f3a8a4347ff4463178a35ea8a8eaa8bb90c1f88b
ErickLuan <erickluan12x@gmail.com>
2026-05-27 22:43:21 -0300

    Create Get by id/orderid

--- payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
@@ -1,16 +1,19 @@
 package com.example.payment_service.controller;
 
 import com.example.payment_service.dto.PaymentRequestDTO;
-import com.example.payment_service.entity.Payment;
-import com.example.payment_service.repository.PaymentRepository;
+import com.example.payment_service.dto.PaymentResponseDTO;
 import com.example.payment_service.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/payments")
@@ -19,8 +22,26 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping
-    public ResponseEntity<Payment> createPayment(@RequestBody PaymentRequestDTO paymentRequestDTO){
-    Payment paymentResponseDTO = paymentService.createPayment(paymentRequestDTO);
-    return ResponseEntity.status(HttpStatus.CREATED).body(paymentResponseDTO);
+    public ResponseEntity<PaymentResponseDTO> createPayment(@RequestBody PaymentRequestDTO paymentRequestDTO){
+    PaymentResponseDTO response = paymentService.createPayment(paymentRequestDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PaymentResponseDTO>> findAllPayments(){
+        List<PaymentResponseDTO> paymentList = paymentService.findAllPayments();
+        return ResponseEntity.ok(paymentList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaymentResponseDTO> findPaymentById(@PathVariable Long id){
+        PaymentResponseDTO payment = paymentService.findPaymentById(id);
+        return ResponseEntity.ok(payment);
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponseDTO> findPaymentByOrderId(@PathVariable Long orderId) {
+        PaymentResponseDTO payment = paymentService.findPaymentByOrderId(orderId);
+        return ResponseEntity.ok(payment);
     }
 }
